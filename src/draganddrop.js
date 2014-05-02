9e512c9e165a20f624d851417a9958081b01be2d
nithril <nithril@gmail.com>
2014-05-02 23:14:45 +0200

    #2 Add support for multi touch screen

--- src/draganddrop.js
+++ src/draganddrop.js
@@ -6,41 +6,81 @@ var app = angular.module('dragAndDrop', []);
  */
 app.controller("DragAndDropItemController", ['$scope', '$element', '$document' , function ($scope, $element, $document) {
 
-    $scope.init = function (data) {
-        $scope.data = data;
+    $scope.init = function (value) {
+        $scope.value = value;
+        $scope.value.$element = $element;
     }
 
-    //dragged item follows mouse
-    var mousemove = function (event) {
-        $element.css({
-            top: event.pageY + 5,
-            left: event.pageX + 5,
-            visibility: "visible"
-        });
-    }
-    $document.bind('mousemove', mousemove);
-
-    $scope.$on("$destroy", function () {
-        $document.unbind('mousemove', mousemove);
-    });
 }]);
 
 /**
  * Controller for all dragged items
  */
-app.controller("DragAndDropController", ['$scope', 'dragAndDrop', '$document' , function ($scope, dragAndDrop, $document) {
+app.controller("DragAndDropController", ['$rootScope' , '$scope', 'dragAndDrop', '$document' , function ($rootScope , $scope, dragAndDrop, $document) {
 
     //mouse up : d&d ended, remove the associated data
     var mouseUp = function (evt) {
+        console.log("mouse up DragAndDropController");
         $scope.$apply(function () {
             dragAndDrop.removeData("mouse");
         });
     }
 
+    var touchEnd = function (evt) {
+        $scope.$apply(function () {
+            for (var ite = 0 ; ite < evt.changedTouches.length ; ite++){
+                var currentTouch = evt.changedTouches[ite];
+                if (dragAndDrop.hasData(currentTouch.identifier)) {
+                    $rootScope.$emit("touchend" , currentTouch);
+                    dragAndDrop.removeData(currentTouch.identifier)
+                }
+            }
+        });
+    }
+
+
+    //dragged item follows mouse
+    var mousemove = function (evt) {
+        /**/
+        if (dragAndDrop.hasData("mouse")) {
+
+            dragAndDrop.getData("mouse").$element.css({
+                top: evt.pageY + 5,
+                left: evt.pageX + 5,
+                visibility: "visible"
+            });
+
+        }
+    }
+    //dragged item follows mouse
+    var touchmove = function (evt) {
+        for (var ite = 0 ; ite < evt.targetTouches.length ; ite++){
+            var currentTouch = evt.targetTouches[ite];
+            if (dragAndDrop.hasData(currentTouch.identifier)) {
+                dragAndDrop.getData(currentTouch.identifier).$element.css({
+                    top: currentTouch.pageY + 5,
+                    left: currentTouch.pageX + 5,
+                    visibility: "visible"
+                });
+                                evt.stopPropagation();
+                                evt.preventDefault();
+            }
+        }
+    }
+
+    $document.bind('mousemove', mousemove);
+    $document.bind('touchmove', touchmove);
+
+    //Mouse up on document to remove d&d which bubbled to document
     $document.bind('mouseup', mouseUp);
+    //Touch end on document to emit Touch to drop directives
+    $document.bind('touchend', touchEnd);
 
     $scope.$on("$destroy", function () {
         $document.unbind('mouseup', mouseUp);
+        $document.unbind('touchend', touchEnd);
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('touchmove', touchmove);
     });
 
     $scope.items = dragAndDrop.datas;
@@ -84,11 +124,22 @@ app.factory('dragAndDrop', function () {
 });
 
 
-app.directive("drop", ['$rootScope', "dragAndDrop", function ($rootScope, dragAndDrop) {
+app.directive("drop", ['$rootScope', "dragAndDrop", "$document", function ($rootScope, dragAndDrop, $document) {
 
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
+        link: function (scope, $element, attrs) {
+
+            function doDrop(identifier, target){
+                if (attrs.dropCallback) {
+                    var result = scope.$eval(attrs.dropCallback);
+                    if (angular.isFunction(result)) {
+                        result(dragAndDrop.getData(identifier).data);
+                    }
+                }
+                dragAndDrop.getData(identifier).callback();
+                dragAndDrop.removeData(identifier);
+            }
 
             var mouseUp = function (evt) {
                 if (!dragAndDrop.hasData("mouse")) {
@@ -97,21 +148,26 @@ app.directive("drop", ['$rootScope', "dragAndDrop", function ($rootScope, dragAn
                 evt.stopPropagation();
                 evt.preventDefault();
                 scope.$apply(function () {
-                    if (attrs.dropCallback) {
-                        var result = scope.$eval(attrs.dropCallback);
-                        if (angular.isFunction(result)) {
-                            result(dragAndDrop.getData("mouse").data);
-                        }
-                    }
-                    dragAndDrop.getData("mouse").callback();
-                    dragAndDrop.removeData("mouse");
+                    doDrop("mouse");
                 });
             }
 
-            element.bind('mouseup', mouseUp);
+            var touchend = function (s,currentTouch) {
+                var touchEndElement = $document[0].elementFromPoint(currentTouch.pageX, currentTouch.pageY);
+                if ($element[0].compareDocumentPosition(touchEndElement) & Node.DOCUMENT_POSITION_CONTAINED_BY) {
+                    doDrop(currentTouch.identifier);
+                }
+            }
+
+
+            $rootScope.$on("touchend" , touchend);
+
+
+            //A mouseup occurs on the target element
+            $element.bind('mouseup', mouseUp);
 
             scope.$on("$destroy", function () {
-                element.unbind('mouseup', mouseUp);
+                $element.unbind('mouseup', mouseUp);
             });
         }
     }
@@ -143,12 +199,25 @@ app.directive("drag", ["$rootScope", "dragAndDrop", "$document", "$controller", 
                     }
                 });
             }
+            var touchstart = function (evt) {
+                evt.preventDefault();
+                scope.$apply(function () {
+                    for (var ite = 0 ; ite < evt.targetTouches.length ; ite++){
+                        var currentTouch = evt.targetTouches[ite];
+                        if (attrs.dragData) {
+                            dragAndDrop.setData(currentTouch.identifier, scope.$eval(attrs.dragData), success, dragInclude, element[0].offsetWidth, element[0].offsetHeight);
+                        }
+                    }
+                });
+            }
 
 
             element.bind('mousedown', mouseDown);
+            element.bind('touchstart', touchstart);
 
             scope.$on("$destroy", function () {
                 element.unbind('mousedown', mouseDown);
+                element.unbind('touchstart', touchstart);
             });
 
         }
